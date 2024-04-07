a9189c25f33cb6594c229f6a4184c1613e4178fd
smbanati-IZxCKm <smbanati@gmail.com>
2024-04-07 01:11:21 -0400

    added GET logic for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -39,6 +39,24 @@ export const putDb = async (content) => {
 
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('get all data from database');
+
+  // Creates connection to the database and the version number
+  const jateDb = await openDB('jate', 1);
+  // Creates a transaction to jate db, enables readonly operations
+  const tx = jateDb.transaction('jate', 'readonly');
+  // Access the object store jate and run objectstore method on tx
+  const store = tx.objectStore('jate');
+  // create request to get all data 
+  const request = store.getAll();
+
+  const result = await request.catch((error) => {
+    console.error('Eror while getting data', error);
+    throw error;
+});
+console.log('result.value', result.value);
+return result.value;
+};
 
 initdb();
